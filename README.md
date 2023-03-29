0c4432f42aa9f1ba51adfdfbf29e410ce822b4cf
xiongjunyan <xjysanjie@163.com>
2023-03-29 13:19:41 +0800

    第一次

---
+++ README.md
@@ -0,0 +1,26 @@
+# 06 初始化
+pnpm create vite
+创建工程
+pnpm run dev
+启动
+
+
+#  代码规范 eslint
+https://git.lingyu.org.cn/learn/naive-admin-pro/wiki/03-代码规范工具
+
+安装
+@mistjs/eslint-config-vue 该项目已经写好的规则
+pnpm add eslint @mistjs/eslint-config-vue -D
+
+新增配置文件.elsintrc
+{
+
+    //继承默认规则
+    "extends":"@mistjs/eslint-config-vue",
+    //个人新增规则
+    "rules":{
+    }
+}
+
+# git hooks husky  提交代码
+husky可以将git内置的钩子暴露出来，在git hooks中导致的无法提交钩子的问题。
---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      // 响应式语法糖
+      reactivityTransform: true
+    }),
+    AutoImport({
+      // 配置自动导入的库
+      imports: [
+        'vue',
+        'vue/macros', // 响应式语法糖库
+        'vue-router', // 路由
+        'vue-i18n', // 多语言
+        'pinia', // 状态管理库
+        '@vueuse/core', // 工具库
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ],
+      // 生成到的地址
+      dts: 'types/auto-imports.d.ts',
+      // 配置本地需要自动导入的库
+      dirs: [
+        // pinia状态管理目录
+        'src/stores',
+        // 自定义组合式api目录
+        'src/composables'
+      ]
+    }),
+    // 组件自动导入
+    Components({
+      // 导入naiveui的配置项目
+      resolvers: [NaiveUiResolver()],
+      // 生成类型的地址
+      dts: 'types/components.d.ts',
+      dirs: ['src/components']
+    })
+  ]
+})
